f606094b2ec4fe934935694aa5a82cf3149e4aad
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2014-02-05 02:30:44 +0200

    Add the beyond engine; Home controller; Add twitter Bootstrap and some work on the design. oooh, there are many things

---
+++ db/migrate/20140204211919_create_beyond_tenants.beyond.rb
@@ -0,0 +1,11 @@
+# This migration comes from beyond (originally 20140204173902)
+class CreateBeyondTenants < ActiveRecord::Migration
+  def change
+    create_table :beyond_tenants do |t|
+      t.string   "name"
+      t.integer  "user_id"
+
+      t.timestamps
+    end
+  end
+end
