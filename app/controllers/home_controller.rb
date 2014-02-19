c7a83f981e4451cbaad096a3255a45e5684669b4
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2014-02-19 22:31:16 +0200

    Fix minor bugs: crash when you try to access the sites page, while not logged in and the bug where create new site was missing while having no sites

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
   end
 
   def sites
-    redirect_to beyond.new_user_session_path, note: 'You have to be logged in to access this part of the site.' unless user_signed_in?
+    redirect_to beyond.new_user_session_path, notice: 'You have to be logged in to access this part of the site.' and return unless user_signed_in?
     @tenants = current_user.tenants
   end
 end
