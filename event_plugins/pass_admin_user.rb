2bb7f3b8a297a26ec2ff0e8f86c1b0e7fd7b1647
mzdravkov <mihail0zdravkov@gmail.com>
2014-05-18 23:52:13 +0300

    meaningful plugin for passing admin user to tenants

--- event_plugins/pass_admin_user.rb
+++ event_plugins/pass_admin_user.rb
@@ -3,7 +3,15 @@ class Test < Plugman::PluginBase
   end
 
   def on_deploy tenant
-    p "Llama!"
-    p tenant.user
+    plugin = """
+      if User.table_exists?
+        unless User.find_by_email('#{tenant.user.email}')
+          User.create({email: '#{tenant.user.email}', encrypted_password: '#{tenant.user.encrypted_password}'})
+        end
+      end
+    """
+    File.open(File.join(ENV['tenants_plugins_dir'], tenant.name, "add_admin.rb"), "w") do |f|
+      f.write(plugin)
+    end
   end
 end
