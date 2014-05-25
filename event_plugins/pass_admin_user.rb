5f5477723018e32f684cefc64471645e75e00875
mzdravkov <mihail0zdravkov@gmail.com>
2014-05-25 19:53:03 +0300

    working plugin for creating user to the tenant app after its creation

--- event_plugins/pass_admin_user.rb
+++ event_plugins/pass_admin_user.rb
@@ -4,12 +4,28 @@ class Test < Plugman::PluginBase
 
   def on_deploy tenant
     plugin = """
-      if User.table_exists?
-        unless User.find_by_email('#{tenant.user.email}')
-          User.create({email: '#{tenant.user.email}', password: '#{tenant.user.encrypted_password}'})
-        end
+PlugMan.define :admin_user do
+  author 'Llama'
+  version '0.0.1'
+  extends ({})
+  requires [:main, :user_profile, :rolify]
+  extension_points []
+  params()
+
+  def start
+    Thread.new do
+      loop do
+        break if User.table_exists? and defined?(Profile) and Profile.table_exists?
       end
-    """
+      user = User.create(email: 'qj@qj.qj', password: 'qjqjqjqj')
+      profile = Profile.new(first_name: 'First Name', last_name: 'Last Name', country: 'Country')
+      user.profile = profile
+      user.profile.save
+      user.save
+    end
+    true
+  end
+end"""
     File.open(File.join(ENV['tenants_plugins_dir'], tenant.name, "add_admin.rb"), "w") do |f|
       f.write(plugin)
     end
